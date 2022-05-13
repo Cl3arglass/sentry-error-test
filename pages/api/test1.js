c1d3e3a83cf8e3293dc19d219234ae3f3bf3fa6b
Cl3arglass <haltaffer@gmail.com>
2022-05-13 11:45:28 -0700

    test1

--- pages/api/test1.js
+++ pages/api/test1.js
@@ -4,7 +4,7 @@ const doAsyncWork = () => Promise.reject(new Error('API Test 1'))
 doAsyncWork()
 
 async function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Deer' })
 }
 
 export default withSentry(handler)
